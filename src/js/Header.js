4a34c0f5bebdb7d3c0c2b5172cf204c315e09fad
GritUrMind <griturmind@gamil.com>
2023-04-08 19:20:44 +0900

    로고 변경

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,7 +1,7 @@
 
 
 import React, { useEffect, useState } from 'react';
-import hospSymbolKrNm from '../img/cbnuh_log1.png'
+import hospSymbolKrNm from '../img/cbnuh_log2.png'
 import '../css/Header.css';
 
 function Header(){
@@ -18,14 +18,18 @@ function Header(){
     return(
         <div className='div-header-main'>
             <div className='div-header-border'>
-                <div className='div-header-logo'>
+                {/* <div className='div-header-logo'>
                     <img className='img-header-logo' src={hospSymbolKrNm} />
-                </div>
+                </div> */}
 
-                <div className='div-header-center'>
-                    <div className='div-header-text'>
-                        사전문진표
+                <div className='div-header-text'>
+                    <div>
+                        <div className='div-header-logo'>
+                            <img className='img-header-logo' src={hospSymbolKrNm} />
+                            <label>충북대학교병원</label>
+                        </div>
                     </div>
+                    <label>사전문진표</label>
                 </div>
             </div>
             
