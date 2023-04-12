305e79b44d1b82ddd76934d82d4eaf0d91452fff
GritYourMind <griturmind@gmail.com>
2023-04-12 18:09:38 +0900

    url 대소문자 구분 없도록 수정

--- src/hooks/getUrlParameter.js
+++ src/hooks/getUrlParameter.js
@@ -4,5 +4,5 @@ export default function getUrlParameter(location,key){
     const params = new URLSearchParams(location.search);
     //console.log("value : " + params.get(key));
 
-    return params.get(key);
+    return params.get(key) == null? params.get(key.toLowerCase()) : params.get(key);
 }
