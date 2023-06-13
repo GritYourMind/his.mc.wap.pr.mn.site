b14360f6b14906a583c419af510bd48aa42182be
GritYourMind <griturmind@gmail.com>
2023-06-13 15:48:56 +0900

    question answer추가요구사항 반영.

--- src/js/QuesionAnswerBody.js
+++ src/js/QuesionAnswerBody.js
@@ -1,26 +1,183 @@
 import {React, useEffect, useState} from 'react';
 import '../App.css'
+import '../css/QuestionAnswerBody.css';
+
+
 
 //질문&답변 목록
-function QuestionAnswerBody({question}){
+function QuestionAnswerBody({question, callback}){
     
+
     const answer = JSON.parse(question.ANS);
+    const [isBottomFixed, setIsBottomFixed] = useState(true);
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+    const [arrChkAns, SetArrChkAns] = useState([]);
+
+
+    
+    useEffect(() => {
+        const handleResize = () => {
+            const newHeight = window.innerHeight;
+
+            console.log("newHeight : " + newHeight + ", windowHeight : " + windowHeight);
+            if (newHeight < windowHeight) {
+                setIsBottomFixed(false);
+            } else {
+                setIsBottomFixed(true);
+            }
+            setWindowHeight(newHeight);
+        };
+
+        const preventClose = (e) => {
+            e.preventDefault();
+            e.returnValue = ""; //Chrome에서 동작하도록; deprecated
+        };
+
+        window.addEventListener('resize', handleResize);
+        //창이 닫히거나, 새로고침
+        window.addEventListener("beforeunload", preventClose);
+      
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            window.removeEventListener("beforeunload", preventClose);
+        };
+        
+      }, []);
+
+
+    useEffect(()=>{
+        
+    },[question]);
+
+    function getInputValue(e) {
+
+        if(e.target.type !== "textarea"){
+        
+            const chkAnsIndex = answer.findIndex(ans => ans.ANS_ID === e.target.value);
+
+            if(chkAnsIndex < 0){
+                // not found answer then error! 
+                //console.log('getInputValue, not found checked value in answer.');
+                return;
+            }
+
+            if(e.target.type === "checkbox" ){
+                //console.log("e.target.type is checkbox.");
+                if(e.target.checked === true){
+                    //console.log("e.target.checked is true. so insert item.");
+                    // checked then insert 
+                    const existsChkAnsIndex = arrChkAns.findIndex(ans => ans.ANS_UNQ_ID === e.target.id);
+
+                    if(existsChkAnsIndex > -1){
+                        // included item
+                        //console.log("included item.");
+                    }else{
+                        // not found item then insert
+                        //console.log("insert into the item.");
+                        const tempUpdatedSaveAnswers = [...arrChkAns];
+                        tempUpdatedSaveAnswers.push({QST_ID: question.QST_ID,
+                                                    ANS_ID: answer[chkAnsIndex].ANS_ID,
+                                                    ANS_UNQ_ID : answer[chkAnsIndex].ANS_UNQ_ID,
+                                                    ANS_CNTE : "",
+                                                    ANS_POST_QST_ID : answer[chkAnsIndex].ANS_POST_QST_ID
+                                                    });
+                        SetArrChkAns(tempUpdatedSaveAnswers);
+                    }
+                }else{
+                    // unchecked then remove
+                    //console.log("e.target.checked is false. so remove item.");
+                    const existsChkAnsIndex = arrChkAns.findIndex(ans => ans.ANS_ID === e.target.value);
+
+                    if(existsChkAnsIndex > -1){
+                        // remove item
+                        //console.log("remove item.");
+                        SetArrChkAns(arrChkAns.filter(ans => ans.ANS_ID !== e.target.value));
+                    }else{
+                        //console.log("not found item.");
+                        // not found item.
+                    }
+                }
+            }
+
+            if(e.target.type === "radio"){
+                const tempUpdatedSaveAnswers = [];
+                tempUpdatedSaveAnswers.push({QST_ID: question.QST_ID,
+                                            ANS_ID: answer[chkAnsIndex].ANS_ID,
+                                            ANS_UNQ_ID : answer[chkAnsIndex].ANS_UNQ_ID,
+                                            ANS_CNTE : e.target.value,
+                                            ANS_POST_QST_ID : answer[chkAnsIndex].ANS_POST_QST_ID
+                                            });
+                SetArrChkAns(tempUpdatedSaveAnswers);
+
+            }
+        }
+        if(e.target.type === "textarea" ){
+
+            //console.log("getInputValue text, input value : " + e.target.value + ", e.target.id : " + e.target.id);
+            const existsChkAnsIndex = arrChkAns.findIndex(ans => ans.ANS_ID === e.target.id);
+
+            const temp = [...arrChkAns];
+            temp[existsChkAnsIndex].ANS_RPY_CNTE = e.target.value;
+            // SetArrChkAns(temp);
+
+        }
+        
+    }
 
     return(
-        <div id={question?.QST_NO} className='inner'>
-            <div>
-                <b>{question?.QST_NO}. {question?.QST_CNTE}</b>
-                
-                {answer?.map(answer=>(
-                    <div className='answer' key={question.QST_ID}>
-                        <input name={question.QST_ID} id={answer.ANS_UNQ_ID} type={'radio'} value={answer.ANS_ID}
-                            onChange={this.props.callback(1)}
-                        />
-                        <label htmlFor={answer.ANS_UNQ_ID}>{answer.ANS_CNTE}</label>
+        <div className='question-outer'>
+            <div id={question?.QST_NO} className='qustionBodyInner'>
+                <div>
+                    <b className='question'>Q. {question?.QST_CNTE}</b>
+                    
+                    <div >
+                        
+                    {answer?.map(ans=>(
+                        <div className='answerText' key={question.ANS_UNQ_ID}>
+                            
+                            {(question.QST_CCLS_CD === "S" ? true : false) &&
+                                <div>
+                                <input name={question.QST_ID} id={ans.ANS_UNQ_ID} type={'radio'} value={ans.ANS_ID} 
+                                     checked={arrChkAns.findIndex(i => i.ANS_UNQ_ID === ans.ANS_UNQ_ID) > -1 ? true : false}
+                                    onChange={getInputValue} />
+                                <label className='lb-question-cnte' htmlFor={ans.ANS_UNQ_ID}>{ans.ANS_CNTE}</label>
+                                </div>
+                             }
+
+                             {(question.QST_CCLS_CD === "M" ? true : false) &&
+                                <div>
+                                    <input name={question.QST_ID} id={ans.ANS_UNQ_ID} type={'checkbox'} value={ans.ANS_ID} 
+                                        checked={arrChkAns.findIndex(i => i.ANS_UNQ_ID === ans.ANS_UNQ_ID) > -1 ? true : false}
+                                        onChange={getInputValue} />
+                                    <label className='lb-question-cnte' htmlFor={ans.ANS_UNQ_ID}>{ans.ANS_CNTE}</label>
+                                </div>
+                             }
+                                
+                            
+                            {(ans.ANS_CCLS_CD === "I" ? true : false) &&  (arrChkAns.findIndex(i => i.ANS_UNQ_ID === ans.ANS_UNQ_ID) > -1 ? true : false) &&
+                            <textarea className='txt-ans-rpy-cnte answerText' id={ans.ANS_ID} type={'text'} 
+                                disabled={arrChkAns.findIndex(i => i.ANS_UNQ_ID === ans.ANS_UNQ_ID) > -1 ? false : true}
+                                onChange={getInputValue}/>
+                                }
+                        </div>
+                        
+                    ))}
+                    
                     </div>
-                ))}
+                </div>
             </div>
+            <div className='div-holizonAlign-center'>
+                <button className={`btn-question-hover ${isBottomFixed ? 'bottom-question-fixed' : ''}`} 
+                onClick={
+                    ()=>{
+                        callback(arrChkAns);
+                    }
+                }>다 음</button>
+            </div>
+            
         </div>
+        
     );
 }
 
--- src/js/QuestionAnswer.js
+++ src/js/QuestionAnswer.js
@@ -1,25 +1,22 @@
 import {useState, useEffect, React} from 'react';
 import '../App.css'
 import getFetchData from '../hooks/getFetchData';
-import { useLocation } from 'react-router-dom';
 import QuestionAnswerBody from './QuesionAnswerBody';
-import { getSuggestedQuery } from '@testing-library/react';
-import { ConsoleView } from 'react-device-detect';
-
+import { useLocation, useNavigate } from 'react-router-dom';
+import '../css/QuestionAnswer.css';
 function QuestionAnswer(){
 
-    let questionIndex = 0;
     const location = useLocation();
-    
-    const [questions, setQuestions] = useState();
+    const navigate = useNavigate(null);
+    const [questions, setQuestions] = useState([]);
     const [question, setQuestion] = useState();
-
-    let submitInterview = [];   // 답변 항목
+    const [submitInterview, SetSubmitInterview] = useState([]);
+    const [ansDoneYN, setAnsDoneYN] = useState("N");
+    const [quesLength, setQuesLength] = useState("0");
+    const [progressRate, setProgressRate] = useState("0");
+    const [postQstID, setPostQstID] = useState("0");
 
     useEffect(() => {
-        
-        console.log("load QuestionAnswer js");
-        console.log("loaction.state.hsp_tp_cd : " + location.state.hsp_tp_cd);
 
         const data = [
             {
@@ -35,12 +32,102 @@ function QuestionAnswer(){
 
         getFetchData(data, (result) => {
             setQuestions(result);
-            setQuestion(result[questionIndex]);
+            //setQuestion(result[0]);
+            setQuesLength(result.length);
         });
     },[]);
 
-    function Callbacktest(testnumber){
-        console.log("callbacktest : " + testnumber);
+    useEffect(() =>{
+        
+        if(ansDoneYN === "Y"){
+            submit();
+        }else{
+            setQuestion(questions[questions.findIndex(item => item.QST_ID === postQstID)]);
+        }
+        
+    },[submitInterview]);
+
+    useEffect(()=>{
+        if(questions.length > 0){
+             setQuestion(questions[0]);
+        }
+
+    },[questions])
+
+    useEffect(() =>{
+        let currentQuesNo;
+        if(question){
+            currentQuesNo = question?.QST_NO;
+        }else{
+            currentQuesNo = "0";
+        }
+        setProgressRate(currentQuesNo);
+    },[question])
+
+    function setAnswer(arrChkAns){
+        
+        let updatedSaveInterview = [];
+
+        arrChkAns.map(removeItem => {
+            updatedSaveInterview = (submitInterview.filter(item => (item.QST_ID+item.ANS_ID) !== (question.QST_ID+removeItem.ANS_ID) ));
+        })
+
+        arrChkAns.map(checkanswer => {
+            
+            if(!checkanswer){
+                return;
+            }
+
+            setAnsDoneYN(checkanswer.ANS_POST_QST_ID === "999" ? "Y" : "N");
+            setPostQstID(checkanswer.ANS_POST_QST_ID);
+            
+            let index = -1;
+
+            if(updatedSaveInterview.length !== 0){
+                index = updatedSaveInterview.findIndex(item => item.QST_ID === question.QST_ID && item.ANS_ID === checkanswer.ANS_ID);
+            }
+            
+            if(index < 0) {
+            
+                updatedSaveInterview.push({QST_ID: question.QST_ID,
+                                            ANS_ID: checkanswer.ANS_ID,
+                                            ANS_CNTE : checkanswer.ANS_CNTE + checkanswer.ANS_RPY_CNTE});
+            }
+        })
+        SetSubmitInterview(updatedSaveInterview);
+    }
+
+
+    //제출하기 버튼 클릭
+    function submit(){
+
+        const data = [
+            {
+                "biz":"HIS.MC.WAP.PR.BIZ.PreMedicalRecordBL",
+                "method":"SavePreInterviewArray",
+                "HSP_TP_CD":"01"
+            }
+        ];
+        submitInterview?.map(p=>(
+            data.push({"IN_ARR_HSP_TP_CD" : location.state.hsp_tp_cd,
+                    "IN_ARR_MDRC_ID" : location.state.mdrc_id,
+                    "IN_ARR_MDRC_FOM_SEQ" : location.state.mdrc_fom_seq,
+                    "IN_ARR_QST_ID" : p.QST_ID,
+                    "IN_ARR_ANS_ID" : p.ANS_ID,
+                    "IN_ARR_ANS_CNTE" : p.ANS_CNTE,
+                    "IN_ARR_FSR_STF_NO" : "TEST",
+                    "IN_ARR_FSR_PRGM_NM" : "사전문진표 작성",
+                    "IN_ARR_FSR_IP_ADDR" : "."})))
+
+        getFetchData(data, (result) => {
+            if (result === undefined || result.length === 0) {
+                navigate("/submit", {state: location.state});
+            }
+            else{
+                alert(result.m_message);
+            }
+        });
+        
     }
 
     if(!question){
@@ -52,8 +139,29 @@ function QuestionAnswer(){
     }
     else{
         return(
-            <div>
-                <QuestionAnswerBody question={question} callback={Callbacktest}/>
+            <div className='div-verticalAlign'>
+                <div id='top'>
+                    <div className='questionTop'>
+                        <div className='div-holizonAlign-left'>
+                            <div className='font-question-top text-left'>진 료 과</div>
+                            <div className='font-question-top'>: {location.state.med_dept_nm}</div>
+                        </div>
+                        <div className='div-holizonAlign-left'>
+                            <div className='font-question-top text-left'>예약일시</div>
+                            <div className='font-question-top'>: {location.state.med_rsv_dtm}</div>
+                        </div>
+                        <div className='div-holizonAlign-left'>
+                            <div className='font-question-top text-left'>성 명</div>
+                            <div className='font-question-top'>: {location.state.pt_nm}</div>
+                        </div>
+
+                        <div className='progress-container'>
+                            <progress value={progressRate} max={quesLength} />
+                            <span>{progressRate}/{quesLength}</span>    
+                        </div>
+                    </div>
+                </div>
+                <QuestionAnswerBody question={question} callback={setAnswer}/>
             </div>
         );
     }
